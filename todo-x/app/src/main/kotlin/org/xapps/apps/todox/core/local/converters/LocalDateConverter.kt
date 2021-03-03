b3f43eec2358e7cb623ae32dd308a81f1d878568
Camilo del Real <cjdelreal2009@gmail.com>
2021-03-03 18:57:13 +0100

    ADD: Room date time converters

--- todo-x/app/src/main/kotlin/org/xapps/apps/todox/core/local/converters/LocalDateConverter.kt
+++ todo-x/app/src/main/kotlin/org/xapps/apps/todox/core/local/converters/LocalDateConverter.kt
@@ -1,4 +1,19 @@
 package org.xapps.apps.todox.core.local.converters
 
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+
 class LocalDateConverter {
+
+    @TypeConverter
+    fun fromLocalDate(value: LocalDate?): String? {
+        return value?.toString()
+    }
+
+    @TypeConverter
+    fun toLocalDate(value: String?): LocalDate? {
+        return value?.let { LocalDate.parse(it) }
+    }
+
 }
--- todo-x/app/src/main/kotlin/org/xapps/apps/todox/core/local/converters/LocalTimeConverter.kt
+++ todo-x/app/src/main/kotlin/org/xapps/apps/todox/core/local/converters/LocalTimeConverter.kt
@@ -1,4 +1,18 @@
 package org.xapps.apps.todox.core.local.converters
 
+import androidx.room.TypeConverter
+import java.time.LocalTime
+
+
 class LocalTimeConverter {
+
+    @TypeConverter
+    fun fromLocalTIme(value: LocalTime?): String? {
+        return value?.toString()
+    }
+
+    @TypeConverter
+    fun toLocalTime(value: String?): LocalTime? {
+        return value?.let { LocalTime.parse(it) }
+    }
 }
